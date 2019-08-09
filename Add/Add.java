1923b0437a60dd49fca5126827e1611fb155b2db
杨丰畅 <ayang818@qq.com>
2019-08-09 20:54:16 +0800

    创建了一个有BUG的项目, add 函数出错

---
+++ Add/Add.java
@@ -0,0 +1,14 @@
+package Add;
+
+/**
+ * Add
+ */
+public class Add {
+    public static int add(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 3));
+    }
+}
